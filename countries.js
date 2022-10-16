8830addde79f6c785466b2ee1da6c994ddb6de73
apu gharami <info.workingforyou@gmail.com>
2022-10-17 01:13:49 +0530

    country flags country name country details

--- countries.js
+++ countries.js
@@ -15,9 +15,28 @@ const displayCountries = countries =>{
         countryDiv.innerHTML =  `
         <h3>Name: ${country.name.common}</h3>
         <h3> capital: ${country.capital ? country.capital [0] : 'no capital'} </h3>
+        <button onclick="loadCountryDetail ('${country.cca2}')">Detals</button>
         `;
         countriesContainer.appendChild(countryDiv);
     })
 }
+
+const loadCountryDetail = (code) => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('ga', code);
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country => {
+    const CountryDetail = document.getElementById('country-detail');
+    CountryDetail.innerHTML = `
+    
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    `
+}
+
+
 
 loadCountries();
